3165ce16e2b7aca55e7419cff2a79d653b60e1ec
zakura0 <simon.kazemi99@gmail.com>
2024-12-04 15:42:44 +0100

    d04 ref

--- d04/d04p1.py
+++ d04/d04p1.py
@@ -5,10 +5,9 @@ for row in range(len(grid)):
     for col in range(len(grid[0])):
         if grid[row][col] == "X":
             for d_row, d_col in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
-                if not (0 <= row + 3 * d_row < len(grid) and 0 <= col + 3 * d_col < len(grid[0])):
-                    continue
-                if grid[row + d_row][col + d_col] == "M" and grid[row + 2 * d_row][col + 2 * d_col] == "A" and grid[row + 3 * d_row][col + 3 * d_col] == "S":
-                    sum += 1
+                if 0 <= row + 3 * d_row < len(grid) and 0 <= col + 3 * d_col < len(grid[0]):
+                    if grid[row + d_row][col + d_col] == "M" and grid[row + 2 * d_row][col + 2 * d_col] == "A" and grid[row + 3 * d_row][col + 3 * d_col] == "S":
+                        sum += 1
 print(sum)
 
 
