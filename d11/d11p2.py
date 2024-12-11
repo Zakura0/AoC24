c925b92253cac969ae46c8d6e318cdf528f9736d
zakura0 <simon.kazemi99@gmail.com>
2024-12-11 21:05:54 +0100

    cleaner

--- d11/d11p2.py
+++ d11/d11p2.py
@@ -12,32 +12,31 @@ def splitEvenInt(int, length):
     left = int // divisor
     right = int % divisor
     return left, right
-    
-def processStone(stone, blinks, cache):
-    if cache is None:
-        cache = {}
+
+cache = {}
+def processStone(stone, blinks):
     if blinks == 0:
         return 1
     blinks = blinks - 1
     if (stone, blinks) in cache:
         return cache[stone, blinks]
     if stone == 0:
-        childs = processStone(1, blinks, cache)
+        childs = processStone(1, blinks)
         cache[stone, blinks] = childs
         return childs
     length = getLength(stone)
     if length % 2 == 0:
         left, right = splitEvenInt(stone, length)
-        childs = processStone(left, blinks, cache) + processStone(right, blinks, cache)
+        childs = processStone(left, blinks) + processStone(right, blinks)
         cache[stone, blinks] = childs
         return childs
-    childs = processStone(stone * 2024, blinks, cache)
+    childs = processStone(stone * 2024, blinks)
     cache[stone, blinks] = childs
     return childs
 
 result = 0
 for stone in stones:
-    result += processStone(stone, 75, cache = None)
+    result += processStone(stone, 75)
 
 print(result)
 
