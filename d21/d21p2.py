0a458a092fe3c6ea94f4395bf567cd8262e5e371
zakura0 <simon.kazemi99@gmail.com>
2024-12-22 03:36:53 +0100

    d21

--- d21/d21p2.py
+++ d21/d21p2.py
@@ -1,4 +1,5 @@
 from itertools import product
+from functools import cache
 with open("input.txt", "r") as file:
     lines = file.read().strip().splitlines()
 
@@ -46,28 +47,45 @@ def getKeypadMoves(keypad):
             key_moves[(key1, key2)] = possible_moves
     return key_moves
 
+big_keypad_moves = getKeypadMoves(big_keypad)
+robot_keypad_moves =  getKeypadMoves(robot_keypad)
+robot_keypad_movelengths = {}
+
 def getAllMoves(keypad_moves, instruction):
     combinations = [keypad_moves[(key1, key2)] for key1, key2 in zip("A" + instruction, instruction)]
     moves = ["".join(combination) for combination in product(*combinations)]
     return moves
 
-result = 0
+for (key1, key2) in robot_keypad_moves.keys():
+    robot_keypad_movelengths[(key1, key2)] = len(robot_keypad_moves[(key1, key2)][0])
 
-big_keypad_moves = getKeypadMoves(big_keypad)
-robot_keypad_moves =  getKeypadMoves(robot_keypad)
+@cache
+def getMoveLength(key1, key2, robot_iteration):
+    if robot_iteration == 1:
+        return robot_keypad_movelengths[(key1, key2)]
+    shortest_path = 1000**10
+    for move in robot_keypad_moves[(key1, key2)]:
+        move_length = 0
+        for k1, k2 in zip("A" + move, move):
+            move_length += getMoveLength(k1, k2, robot_iteration - 1)
+        shortest_path = min(shortest_path, move_length)
+    return shortest_path
+
+result = 0
 
 for line in lines:
     robot_moves = getAllMoves(big_keypad_moves, line)
-    for i in range(2):
-        maybe_moves = []
-        for move in robot_moves:
-            maybe_moves += getAllMoves(robot_keypad_moves, move)
-        
-        min_length = min(len(move) for move in maybe_moves)
-        robot_moves = [move for move in maybe_moves if len(move) == min_length]
+    shortest_length = 1000**10
+    for move in robot_moves:
+        move_length = 0
+        for key1, key2 in zip("A" + move, move):
+            move_length += getMoveLength(key1, key2, 25)
+        shortest_length = min(shortest_length, move_length)
+    
     numeric_part = int(''.join(filter(str.isdigit, line)))
-    result += min_length * numeric_part
+    result += shortest_length * numeric_part
 print(result)
+
 
 
     
